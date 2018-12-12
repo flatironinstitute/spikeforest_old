116575de1af0d012bcfbc3f327be448adffb9574
Jeremy Magland <jeremy.magland@gmail.com>
2018-12-12 18:30:00 -0500

    --parallel option for sf_run_batch

--- run2.sh
+++ run2.sh
@@ -6,18 +6,15 @@ export NUMEXPR_NUM_THREADS=$NUM_WORKERS
 export OMP_NUM_THREADS=$NUM_WORKERS
 
 OPTS="--run_prefix \"srun -c 2 -n 40\""
-echo $OPTS
 #OPTS=""
 
-#bin/sf_run_batch ms4_magland_synth $OPTS
+#eval "bin/sf_run_batch ms4_magland_synth $OPTS"
 
-#bin/sf_run_batch ms4_bionet8c $OPTS
+#eval "bin/sf_run_batch ms4_bionet8c $OPTS"
 
 eval "bin/sf_run_batch ms4_bionet32c $OPTS"
 
-exit
+eval "bin/sf_run_batch ms4_mearec_tetrode $OPTS"
 
-bin/sf_run_batch ms4_mearec_tetrode $OPTS
-
-bin/sf_run_batch ms4_mearec_neuronexus $OPTS
+eval "bin/sf_run_batch ms4_mearec_neuronexus $OPTS"
 
