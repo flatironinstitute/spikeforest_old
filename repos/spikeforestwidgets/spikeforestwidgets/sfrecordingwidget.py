7e022fc14db5650dc864ce58966434411892947e
Jeremy Magland <jeremy.magland@gmail.com>
2019-01-11 12:05:34 -0500

    update

--- repos/spikeforestwidgets/spikeforestwidgets/sfrecordingwidget.py
+++ repos/spikeforestwidgets/spikeforestwidgets/sfrecordingwidget.py
@@ -35,7 +35,7 @@ class ImageView(vd.Component):
         vd.devel.loadJavascript(js=js,delay=1)
         return elmt
 
-class TrueUnitsWidget(vd.Component):
+class TrueUnitsTable(vd.Component):
     def __init__(self,*,true_units_info):
         vd.Component.__init__(self)
         self._true_units_info=true_units_info
@@ -55,6 +55,35 @@ class TrueUnitsWidget(vd.Component):
                 vd.td(str(unit['peak_channel'])),
                 vd.td(str(unit['num_events'])),
                 vd.td(str(unit['firing_rate']))
+            ))
+        table=vd.table(rows,class_='table')
+        return vd.div(ScrollArea(vd.div(table),height=400))
+
+class ComparisonWithTruthTable(vd.Component):
+    def __init__(self,comparison_info):
+        vd.Component.__init__(self)
+        self._comparison_info=comparison_info
+    def render(self):
+        rows=[]
+        rows.append(vd.tr(
+            vd.th('Unit ID'),
+            vd.th('Accuracy'),
+            vd.th('Best unit'),
+            vd.th('Matched unit'),
+            vd.th('Num. matches'),
+            vd.th('False negative rate'),
+            vd.th('False positive rate')
+        ))
+        for ii in self._comparison_info:
+            unit=self._comparison_info[ii]
+            rows.append(vd.tr(
+                vd.td(str(unit['unit_id'])),
+                vd.td(str(unit['accuracy'])),
+                vd.td(str(unit['best_unit'])),
+                vd.td(str(unit['matched_unit'])),
+                vd.td(str(unit['num_matches'])),
+                vd.td(str(unit['f_n'])),
+                vd.td(str(unit['f_p']))
             ))
         table=vd.table(rows,class_='table')
         return vd.div(ScrollArea(vd.div(table),height=400))
@@ -97,16 +126,32 @@ def save_plot(fname,quality=40):
     os.remove(fname+'.png')
     im.save(fname,quality=quality)
 
+class ButtonBar(vd.Component):
+    def __init__(self,data):
+        vd.Component.__init__(self)
+        self._data=data
+    def render(self):
+        button_style={'margin':'3px'}
+        buttons=[
+            vd.button(item[0],onclick=item[1],style=button_style)
+            for item in self._data
+        ]
+        return vd.div(buttons,style={'padding-bottom':'20px'})
+
 class SFRecordingWidget(vd.Component):
   def __init__(self):
     vd.Component.__init__(self)
     self._recording=None
+    self._sorting_result_name=None
     self._view=None
     vd.devel.loadBootstrap()
   def setRecording(self,recording):
     self._recording=recording
     self._view=None
     #self._timeseries_widget=
+    self.refresh()
+  def setSortingResultName(self,name):
+    self._sorting_result_name=name
     self.refresh()
   def _on_view_timeseries(self):
     rx=self._recording.recordingExtractor()
@@ -124,7 +169,7 @@ class SFRecordingWidget(vd.Component):
     self.refresh()
   def _on_view_true_units(self):
     info=self._recording.trueUnitsInfo(format='json')
-    self._view=TrueUnitsWidget(true_units_info=info)
+    self._view=TrueUnitsTable(true_units_info=info)
     self.refresh()
   def _on_view_true_unit_waveforms(self):
     rx=self._recording.recordingExtractor()
@@ -148,6 +193,18 @@ class SFRecordingWidget(vd.Component):
     self.refresh()
   def _on_download_firings_true_file(self):
     self._recording.realizeFiringsTrueFile()
+    self.refresh()
+  def _on_view_comparison_with_truth_table(self):
+    res=self._recording.sortingResult(self._sorting_result_name)
+    info=res.comparisonWithTruth(format='json')
+    self._view=ComparisonWithTruthTable(info)
+    self.refresh()
+  def _on_view_sorted_unit_waveforms(self):
+    rx=self._recording.recordingExtractor()
+    rx=st.preprocessing.bandpass_filter(recording=rx,freq_min=300,freq_max=6000)
+    res=self._recording.sortingResult(self._sorting_result_name)
+    sx=res.sorting()
+    self._view=SFW.UnitWaveformsWidget(recording=rx,sorting=sx)
     self.refresh()
   def render(self):
     if not self._recording:
@@ -193,24 +250,39 @@ class SFRecordingWidget(vd.Component):
         vd.th('firings_true.mda is downloaded'),vd.td(elmt))
     )
 
+    res=None
+    if self._sorting_result_name:
+        res=self._recording.sortingResult(self._sorting_result_name)
+
+    if res:
+        rows.append(vd.tr(
+            vd.th('Sorting result'),vd.td(self._sorting_result_name)
+        ))
+        sorting=res.sorting()
+        rows.append(vd.tr(
+            vd.th('Num. sorted units'),vd.td('{}'.format(len(sorting.getUnitIds())))
+        ))
+
     table=vd.table(rows,style={'text-align':'left','width':'auto','font-size':'13px'},class_='table')
-    button_style={'margin':'3px'}
-    view_timeseries_button=vd.button('View timeseries',onclick=self._on_view_timeseries,style=button_style)
-    view_geometry_button=vd.button('View electrode geometry',onclick=self._on_view_geometry,style=button_style)
-    view_true_units_button=vd.button('View true unit table',onclick=self._on_view_true_units,style=button_style)
-    view_true_unit_waveforms_button=vd.button('View true unit waveforms',onclick=self._on_view_true_unit_waveforms,style=button_style)
-    view_true_unit_autocorrelograms_button=vd.button('View true unit autocorrelograms',onclick=self._on_view_true_unit_autocorrelograms,style=button_style)
-    button_bar=vd.div(
-        view_timeseries_button,
-        view_geometry_button,
-        view_true_units_button,
-        view_true_unit_waveforms_button,
-        view_true_unit_autocorrelograms_button,
-        style={'padding-bottom':'20px'}
-    )
+
+    button_bar=ButtonBar([
+        ('View timeseries',self._on_view_timeseries),
+        ('View electrode geometry',self._on_view_geometry),
+        ('View true unit table',self._on_view_true_units),
+        ('View true unit waveforms',self._on_view_true_unit_waveforms),
+        ('View true unit autocorrelograms',self._on_view_true_unit_autocorrelograms)
+    ])
     elmts=[table,button_bar]
+
+    if res:
+        button_bar_result=ButtonBar([
+            ('View comparison with truth table',self._on_view_comparison_with_truth_table),
+            ('View sorted unit waveforms',self._on_view_sorted_unit_waveforms)
+        ])    
+        elmts.append(button_bar_result)
     
     if self._view:
         elmts.append(self._view)
+
     return vd.div(elmts)
 
