eafaa31f58849b774cd09b332775ee20eabe0e18
Jeremy Magland <jeremy.magland@gmail.com>
2019-01-30 22:57:58 +0000

    update gitpod init

--- gitpod_init.sh
+++ gitpod_init.sh
@@ -5,16 +5,16 @@ pip install -r requirements.txt
 python setup.py develop
 pip install jupyterlab
 
-if [ -d /home/theiapod ]; then
-    echo "test"
-    for filename in /home/theiapod/{*,.??*}; do
-        echo "testing --- $filename"
-        if [ -e "$filename" ]; then # in case no such file exists
-            fname=$(basename $filename)
-            ln -s /home/theiapod/$fname ~/$fname
-        fi
-    done
-fi
+#if [ -d /home/theiapod ]; then
+#    echo "test"
+#    for filename in /home/theiapod/{*,.??*}; do
+#        echo "testing --- $filename"
+#        if [ -e "$filename" ]; then # in case no such file exists
+#            fname=$(basename $filename)
+#            ln -s /home/theiapod/$fname ~/$fname
+#        fi
+#    done
+#fi
 
 #if [ -f /home_data/.gitconfig ]; then
 #    ln -s /home_data/.gitconfig ~/.gitconfig
