d4bb779ba7757ea34d7076365f39267e7623afb0
Jeremy Magland <jeremy.magland@gmail.com>
2019-01-31 20:23:49 +0000

    study sorter figure for sfbrowser

--- pages/sfbrowser/sfbrowser.py
+++ pages/sfbrowser/sfbrowser.py
@@ -3,6 +3,50 @@ import spikeforest as sf
 from kbucket import client as kb
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+
+
+class AccuracyPlot(vd.components.Pyplot):
+    def __init__(self,snrs,accuracies):
+        vd.components.Pyplot.__init__(self)
+        self._snrs=snrs
+        self._accuracies=accuracies
+
+    def plot(self):
+        plt.scatter(self._snrs,self._accuracies)
+
+
+class StudySorterFigure(vd.Component):
+    def __init__(self,sfdata):
+      vd.Component.__init__(self)
+      self._plot=None
+      self._SF_data=sfdata
+      self._study=None
+      self._sorter=None
+
+    def setStudySorter(self,*,study,sorter):
+        self._study=study
+        self._sorter=sorter
+        self._update_plot()
+
+    def _update_plot(self):
+        SF=self._SF_data
+        study=SF.study(self._study)
+        b=_get_study_sorting_results(study)
+        a=b[self._sorter]
+        snrs=a['true_unit_snrs']
+        accuracies=a['num_matches']/(a['num_matches']+a['num_false_positives']+a['num_false_negatives'])
+
+        self._plot=AccuracyPlot(snrs,accuracies)
+        self.refresh()
+    
+    def render(self):
+        if self._plot is None:
+            return vd.div('Nothing')
+        return vd.div(
+            vd.div('test '+self._study+' '+self._sorter),
+            self._plot
+        )
 
 class SFBrowser(vd.Component):
   def __init__(self,group):
@@ -25,6 +69,8 @@ class SFBrowser(vd.Component):
     self._SF_data=SF
     self._accuracy_threshold_input=vd.components.LineEdit(value=0.8,dtype=float,style=dict(width='70px'))
     self._update_button=vd.components.Button(onclick=self._on_update,class_='button',label='Update')
+    self._study_sorter_fig=StudySorterFigure(SF)
+    self._study_sorter_table=vd.div() #dummy
 
     vd.devel.loadBootstrap()
 
@@ -35,14 +81,61 @@ class SFBrowser(vd.Component):
 
   def _update_accuracy_table(self):
     accuracy_threshold=self._accuracy_threshold_input.value()
-    self._accuracy_table, self._sorters=_get_accuracy_table(self._SF_data,
-    accuracy_threshold=accuracy_threshold)
+    self._accuracy_table_data, self._sorters=self._get_accuracy_table_data(accuracy_threshold=accuracy_threshold)
+    self._accuracy_table=self._to_table(self._accuracy_table_data,['study']+self._sorters)    
     self.refresh()
+
+  def _open_study_sorter_fig(self,*,sorter,study):
+    self._study_sorter_fig.setStudySorter(study=study,sorter=sorter)
+
+  def _get_accuracy_table_data(self,*,accuracy_threshold):
+    SF=self._SF_data
+    accuracy_table=[]
+    sorters=set()
+    for sname in SF.studyNames():
+        print ('STUDY: '+sname)
+        study=SF.study(sname)
+        b=_get_study_sorting_results(study)
+        tmp=dict(
+            study=dict( # first column
+                text=sname
+            )
+        )
+        for sorter in b:
+            sorters.add(sorter)
+            a=b[sorter]
+            accuracies=a['num_matches']/(a['num_matches']+a['num_false_positives']+a['num_false_negatives'])
+            tmp[sorter]=dict(
+                text=str(np.count_nonzero(accuracies>=accuracy_threshold)),
+                callback=lambda sorter=sorter,study=sname: self._open_study_sorter_fig(sorter=sorter,study=study)
+            )
+        accuracy_table.append(tmp)
+    
+    sorters=list(sorters)
+    sorters.sort()
+    return accuracy_table, sorters
+
+  def _to_table(self,X,column_names):
+    rows=[]
+    rows.append(vd.tr([vd.th(cname) for cname in column_names]))
+    for x in X:
+        elmts=[]
+        for cname in column_names:
+            tmp=x.get(cname)
+            if tmp:
+                if 'callback' in tmp:
+                    elmt=vd.a(tmp['text'],onclick=tmp['callback'])
+                else:
+                    elmt=vd.span(tmp['text'])
+            else:
+                elmt=vd.span('N/A')
+            elmts.append(elmt)
+        rows.append(vd.tr([vd.td(elmt) for elmt in elmts]))
+    return vd.table(rows,class_='table')
     
   def render(self):
     SF=self._SF_data
 
-    table=_to_table(self._accuracy_table,['study']+self._sorters)
     return vd.div(
         vd.table(
             vd.tr(
@@ -53,16 +146,15 @@ class SFBrowser(vd.Component):
             class_='table',
             style={'max-width':'200px'}
         ),
-        table,
+        vd.components.ScrollArea(
+            self._accuracy_table,
+            height=500
+        ),
+        self._study_sorter_fig,
         style=dict(padding='15px')
     )
 
-def _to_table(X,column_names):
-    rows=[]
-    rows.append(vd.tr([vd.th(cname) for cname in column_names]))
-    for x in X:
-        rows.append(vd.tr([vd.td(str(x.get(cname))) for cname in column_names]))
-    return vd.table(rows,class_='table')
+
 
 def _get_study_sorting_results(study):
   results=[]
@@ -118,22 +210,3 @@ def _get_study_sorting_results(study):
     
   return ret
       
-def _get_accuracy_table(SF_data,*,accuracy_threshold):
-    SF=SF_data
-    accuracy_table=[]
-    sorters=set()
-    for sname in SF.studyNames():
-        print ('STUDY: '+sname)
-        study=SF.study(sname)
-        b=_get_study_sorting_results(study)
-        tmp=dict(study=sname)
-        for sorter in b:
-            sorters.add(sorter)
-            a=b[sorter]
-            accuracies=a['num_matches']/(a['num_matches']+a['num_false_positives']+a['num_false_negatives'])
-            tmp[sorter]=np.count_nonzero(accuracies>=accuracy_threshold)
-        accuracy_table.append(tmp)
-    
-    sorters=list(sorters)
-    sorters.sort()
-    return accuracy_table, sorters
