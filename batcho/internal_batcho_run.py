d7be3527f82d05d7e9b6673b29f4a3c4150c6eb3
Jeremy Magland <jeremy.magland@gmail.com>
2019-02-02 11:20:29 +0000

    autopep8 formatting

--- batcho/internal_batcho_run.py
+++ batcho/internal_batcho_run.py
@@ -7,41 +7,44 @@ import spikeforest as sf
 import batcho
 import time
 from kbucket import client as kb
-import mlprocessors as mlpr ## needed to register the mlprocessors batcho commands
+import mlprocessors as mlpr  # needed to register the mlprocessors batcho commands
 import traceback
 
 # This can be important for some of the jobs in certain situations
-os.environ['DISPLAY']=''
+os.environ['DISPLAY'] = ''
+
 
 def main():
-  parser = argparse.ArgumentParser(description = 'Listen for batches as a compute resource')
-  parser.add_argument('batch_name',help='Name of the batch to run')
+    parser = argparse.ArgumentParser(
+        description='Listen for batches as a compute resource')
+    parser.add_argument('batch_name', help='Name of the batch to run')
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  BATCHO_CONFIG_NAME=os.environ.get('BATCHO_CONFIG_NAME',None)
-  BATCHO_CONFIG_COLLECTION=os.environ.get('BATCHO_CONFIG_COLLECTION',None)
-  BATCHO_CONFIG_PASSWORD=os.environ.get('BATCHO_CONFIG_PASSWORD',None)
-  BATCHO_LOCAL=os.environ.get('BATCHO_LOCAL',None)
+    BATCHO_CONFIG_NAME = os.environ.get('BATCHO_CONFIG_NAME', None)
+    BATCHO_CONFIG_COLLECTION = os.environ.get('BATCHO_CONFIG_COLLECTION', None)
+    BATCHO_CONFIG_PASSWORD = os.environ.get('BATCHO_CONFIG_PASSWORD', None)
+    BATCHO_LOCAL = os.environ.get('BATCHO_LOCAL', None)
 
-  if (BATCHO_LOCAL=='true') or (BATCHO_CONFIG_NAME and BATCHO_CONFIG_COLLECTION and BATCHO_CONFIG_PASSWORD):
-      pass
-  else:
-      print('You must set the environment variables: BATCHO_CONFIG_NAME, BATCHO_CONFIG_COLLECTION, BATCHO_CONFIG_PASSWORD, or set BATCHO_LOCAL=true')
-      sys.exit(-1)
+    if (BATCHO_LOCAL == 'true') or (BATCHO_CONFIG_NAME and BATCHO_CONFIG_COLLECTION and BATCHO_CONFIG_PASSWORD):
+        pass
+    else:
+        print('You must set the environment variables: BATCHO_CONFIG_NAME, BATCHO_CONFIG_COLLECTION, BATCHO_CONFIG_PASSWORD, or set BATCHO_LOCAL=true')
+        sys.exit(-1)
 
-  if BATCHO_LOCAL=='true':
-      sf.kbucketConfigLocal()
-  else:
-    sf.kbucketConfigRemote(name=BATCHO_CONFIG_NAME,collection=BATCHO_CONFIG_COLLECTION,password=BATCHO_CONFIG_PASSWORD)
+    if BATCHO_LOCAL == 'true':
+        sf.kbucketConfigLocal()
+    else:
+        sf.kbucketConfigRemote(
+            name=BATCHO_CONFIG_NAME, collection=BATCHO_CONFIG_COLLECTION, password=BATCHO_CONFIG_PASSWORD)
 
-  try:
-    batcho.run_batch(batch_name=args.batch_name)
-  except Exception as err:
-    traceback.print_exc()
-    print('Error running batch:',err)
-    sys.exit(-1)
+    try:
+        batcho.run_batch(batch_name=args.batch_name)
+    except Exception as err:
+        traceback.print_exc()
+        print('Error running batch:', err)
+        sys.exit(-1)
 
-if __name__== "__main__":
-  main()
-  
+
+if __name__ == "__main__":
+    main()
