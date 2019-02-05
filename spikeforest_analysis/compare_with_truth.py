49b617fa1b273292f32ffdf1b43aa9108cb3166f
Jeremy Magland <jeremy.magland@gmail.com>
2019-02-05 10:51:15 +0000

    add spikeforest_analysis and set up nbstripout

---
+++ spikeforest_analysis/compare_with_truth.py
@@ -0,0 +1,108 @@
+import spikeextractors as si
+#import spikewidgets as sw
+import spiketoolkit as st
+import mlprocessors as mlpr
+import json
+from kbucket import client as kb
+import numpy as np
+
+def compare_with_truth(sorting):
+    ret={}
+    units_true=sorting.get('units_true',[])
+    out=GenSortingComparisonTable.execute(firings=sorting['firings'],firings_true=sorting['firings_true'],units_true=units_true,json_out={'ext':'.json'},html_out={'ext':'.html'}).outputs
+    ret['json']=kb.saveFile(out['json_out'],basename='table.json')
+    ret['html']=kb.saveFile(out['html_out'],basename='table.html')
+    return ret
+
+class GenSortingComparisonTable(mlpr.Processor):
+    VERSION='0.2.0'
+    firings=mlpr.Input('Firings file (sorting)')
+    firings_true=mlpr.Input('True firings file')
+    units_true=mlpr.IntegerListParameter('List of true units to consider')
+    json_out=mlpr.Output('Table as .json file produced from pandas dataframe')
+    html_out=mlpr.Output('Table as .html file produced from pandas dataframe')
+    
+    def run(self):
+        sorting=si.MdaSortingExtractor(firings_file=self.firings)
+        sorting_true=si.MdaSortingExtractor(firings_file=self.firings_true)
+        if len(self.units_true)>0:
+            sorting_true=si.SubSortingExtractor(parent_sorting=sorting_true,unit_ids=self.units_true)
+        SC=st.comparison.SortingComparison(sorting_true,sorting)
+        df=get_comparison_data_frame(comparison=SC)
+        #sw.SortingComparisonTable(comparison=SC).getDataframe()
+        json=df.transpose().to_dict()
+        html=df.to_html(index=False)
+        _write_json_file(json,self.json_out)
+        _write_json_file(html,self.html_out)
+
+def get_comparison_data_frame(*,comparison):
+    import pandas as pd
+    SC=comparison
+
+    unit_properties=[] #snr, etc? these would need to be properties in the sortings of the comparison
+
+    # Compute events counts
+    sorting1=SC.getSorting1()
+    sorting2=SC.getSorting2()
+    unit1_ids = sorting1.getUnitIds()
+    unit2_ids = sorting2.getUnitIds()
+    N1 = len(unit1_ids)
+    N2 = len(unit2_ids)
+    event_counts1 = dict()
+    for i1, u1 in enumerate(unit1_ids):
+        times1 = sorting1.getUnitSpikeTrain(u1)
+        event_counts1[u1] = len(times1)
+    event_counts2 = dict()
+    for i2, u2 in enumerate(unit2_ids):
+        times2 = sorting2.getUnitSpikeTrain(u2)
+        event_counts2[u2] = len(times2)
+
+    rows = []
+    for u_1, unit1 in enumerate(unit1_ids):
+        unit2 = SC.getBestUnitMatch1(unit1)
+        if unit2>=0:
+            num_matches=SC.getMatchingEventCount(unit1, unit2)
+            num_false_negatives=event_counts1[unit1]-num_matches
+            num_false_positives=event_counts2[unit2]-num_matches
+        else:
+            num_matches=0
+            num_false_negatives=event_counts1[unit1]
+            num_false_positives=0
+        row0 = {
+            'unit_id': unit1,
+            'accuracy': _safe_frac(num_matches,num_false_positives+num_false_negatives+num_matches),
+            'best_unit': unit2,
+            'matched_unit': SC.getMappedSorting1().getMappedUnitIds(unit1),
+            'num_matches': num_matches,
+            'num_false_negatives': num_false_negatives,
+            'num_false_positives': num_false_positives,
+            'f_n': _safe_frac(num_false_negatives,num_false_negatives+num_matches),
+            'f_p': _safe_frac(num_false_positives,num_false_positives+num_matches)
+        }
+        for prop in unit_properties:
+            pname = prop['name']
+            row0[pname] = SC.getSorting1().getUnitProperty(unit_id=int(unit1), property_name=pname)
+        rows.append(row0)
+
+    df = pd.DataFrame(rows)
+    fields = ['unit_id']
+    fields = fields + ['accuracy', 'best_unit', 'matched_unit', 'num_matches', 'num_false_negatives', 'num_false_positives', 'f_n', 'f_p']
+    for prop in unit_properties:
+        pname = prop['name']
+        fields.append(pname)
+    df = df[fields]
+    df['accuracy'] = df['accuracy'].map('{:,.4f}'.format)
+    # df['Best match'] = df['Accuracy'].map('{:,.2f}'.format)
+    df['f_n'] = df['f_n'].map('{:,.4f}'.format)
+    df['f_p'] = df['f_p'].map('{:,.4f}'.format)
+    return df
+
+def _safe_frac(numer, denom):
+    if denom == 0:
+        return 0
+    return float(numer) / denom
+
+def _write_json_file(obj,path):
+  with open(path,'w') as f:
+    return json.dump(obj,f)
+
---
+++ spikeforest_analysis/sort_recording.py
@@ -0,0 +1,221 @@
+from kbucket import client as kb
+import spikeextractors as si
+import mlprocessors as mlpr
+import os
+import shutil
+import random
+import string
+from . import sorters as sorters
+
+from spikesorters import MountainSort4, SpykingCircus
+    
+
+class IronClust(mlpr.Processor):
+    NAME='IronClust'
+    VERSION='4.2.8'
+    
+    recording_dir=mlpr.Input('Directory of recording',directory=True)
+    channels=mlpr.IntegerListParameter(description='List of channels to use.',optional=True,default=[])
+    firings_out=mlpr.Output('Output firings file')
+    
+    detect_sign=mlpr.IntegerParameter('Use -1, 0, or 1, depending on the sign of the spikes in the recording')
+    adjacency_radius=mlpr.FloatParameter('Use -1 to include all channels in every neighborhood')
+    detect_threshold=mlpr.FloatParameter(optional=True,default=3,description='')
+    prm_template_name=mlpr.StringParameter(optional=False,description='TODO')
+    freq_min=mlpr.FloatParameter(optional=True,default=300,description='Use 0 for no bandpass filtering')
+    freq_max=mlpr.FloatParameter(optional=True,default=6000,description='Use 0 for no bandpass filtering')
+    merge_thresh=mlpr.FloatParameter(optional=True,default=0.98,description='TODO')
+    pc_per_chan=mlpr.IntegerParameter(optional=True,default=3,description='TODO')
+    
+    def run(self):
+        ironclust_src=os.environ.get('IRONCLUST_SRC',None)
+        if not ironclust_src:
+            raise Exception('Environment variable not set: IRONCLUST_SRC')
+        code=''.join(random.choice(string.ascii_uppercase) for x in range(10))
+        tmpdir=os.environ.get('TEMPDIR','/tmp')+'/ironclust-tmp-'+code
+            
+        try:
+            recording=si.MdaRecordingExtractor(self.recording_dir)
+            if len(self.channels)>0:
+              recording=si.SubRecordingExtractor(parent_recording=recording,channel_ids=self.channels)
+            if not os.path.exists(tmpdir):
+                os.mkdir(tmpdir)
+            sorting=sorters.ironclust(
+                recording=recording,
+                tmpdir=tmpdir, ## TODO
+                detect_sign=self.detect_sign,
+                adjacency_radius=self.adjacency_radius,
+                detect_threshold=self.detect_threshold,
+                merge_thresh=self.merge_thresh,
+                freq_min=self.freq_min,
+                freq_max=self.freq_max,
+                pc_per_chan=self.pc_per_chan,
+                prm_template_name=self.prm_template_name,
+                ironclust_src=ironclust_src
+            )
+            si.MdaSortingExtractor.writeSorting(sorting=sorting,save_path=self.firings_out)
+        except:
+            if os.path.exists(tmpdir):
+                shutil.rmtree(tmpdir)
+            raise
+        shutil.rmtree(tmpdir)
+        
+# class SpykingCircus(mlpr.Processor):
+#     NAME='SpykingCircus'
+#     VERSION='0.1.7'
+    
+#     recording_dir=mlpr.Input('Directory of recording',directory=True)
+#     #singularity_container=mlpr.Input('Singularity container',optional=False)
+#     channels=mlpr.IntegerListParameter(description='List of channels to use.',optional=True,default=[])
+#     firings_out=mlpr.Output('Output firings file')
+    
+#     detect_sign=mlpr.IntegerParameter(description='-1, 1, or 0')
+#     adjacency_radius=mlpr.FloatParameter(optional=True,default=100,description='Channel neighborhood adjacency radius corresponding to geom file')
+#     spike_thresh=mlpr.FloatParameter(optional=True,default=6,description='Threshold for detection')
+#     template_width_ms=mlpr.FloatParameter(optional=True,default=3,description='Spyking circus parameter')
+#     filter=mlpr.BoolParameter(optional=True,default=True)
+#     whitening_max_elts=mlpr.IntegerParameter(optional=True,default=1000,description='I believe it relates to subsampling and affects compute time')
+#     clustering_max_elts=mlpr.IntegerParameter(optional=True,default=10000,description='I believe it relates to subsampling and affects compute time')
+
+#     def run(self):
+#         singularity_container=os.environ.get('SC_SINGULARITY_CONTAINER',None)
+#         if not singularity_container:
+#             raise Exception('You must set the environment variable SC_SINGULARITY_CONTAINER.')
+
+#         code=''.join(random.choice(string.ascii_uppercase) for x in range(10))
+#         tmpdir=os.environ.get('TEMPDIR','/tmp')+'/ironclust-tmp-'+code
+        
+#         num_workers=os.environ.get('NUM_WORKERS',1)
+            
+#         try:
+#             recording=si.MdaRecordingExtractor(self.recording_dir)
+#             if len(self.channels)>0:
+#               recording=si.SubRecordingExtractor(parent_recording=recording,channel_ids=self.channels)
+#             if not os.path.exists(tmpdir):
+#                 os.mkdir(tmpdir)
+#             sorting=sorters.spyking_circus(
+#                 recording=recording,
+#                 output_folder=tmpdir,
+#                 probe_file=None,
+#                 file_name=None,
+#                 detect_sign=self.detect_sign,
+#                 adjacency_radius=self.adjacency_radius,
+#                 spike_thresh=self.spike_thresh,
+#                 template_width_ms=self.template_width_ms,
+#                 filter=self.filter,
+#                 merge_spikes=True,
+#                 n_cores=num_workers,
+#                 electrode_dimensions=None,
+#                 whitening_max_elts=self.whitening_max_elts,
+#                 clustering_max_elts=self.clustering_max_elts,
+#                 singularity_container=singularity_container
+#             )
+#             si.MdaSortingExtractor.writeSorting(sorting=sorting,save_path=self.firings_out)
+#         except:
+#             if os.path.exists(tmpdir):
+#                 shutil.rmtree(tmpdir)
+#             raise
+#         shutil.rmtree(tmpdir)
+
+class KiloSort(mlpr.Processor):
+    NAME='KiloSort'
+    VERSION='0.1.0' # wrapper version
+    
+    recording_dir=mlpr.Input('Directory of recording',directory=True)
+    channels=mlpr.IntegerListParameter(description='List of channels to use.',optional=True,default=[])
+    firings_out=mlpr.Output('Output firings file')
+    
+    detect_sign=mlpr.IntegerParameter('Use -1 or 1, depending on the sign of the spikes in the recording')
+    adjacency_radius=mlpr.FloatParameter('Use -1 to include all channels in every neighborhood')
+    detect_threshold=mlpr.FloatParameter(optional=True,default=3,description='')
+    #prm_template_name=mlpr.StringParameter(optional=False,description='TODO')
+    freq_min=mlpr.FloatParameter(optional=True,default=300,description='Use 0 for no bandpass filtering')
+    freq_max=mlpr.FloatParameter(optional=True,default=6000,description='Use 0 for no bandpass filtering')
+    merge_thresh=mlpr.FloatParameter(optional=True,default=0.98,description='TODO')
+    pc_per_chan=mlpr.IntegerParameter(optional=True,default=3,description='TODO')
+    
+    def run(self):
+        code=''.join(random.choice(string.ascii_uppercase) for x in range(10))
+        tmpdir=os.environ.get('TEMPDIR','/tmp')+'/kilosort-tmp-'+code
+            
+        try:
+            recording=si.MdaRecordingExtractor(self.recording_dir)
+            if len(self.channels)>0:
+              recording=si.SubRecordingExtractor(parent_recording=recording,channel_ids=self.channels)
+            if not os.path.exists(tmpdir):
+                os.mkdir(tmpdir)
+            sorting=sorters.kilosort(
+                recording=recording,
+                tmpdir=tmpdir, 
+                detect_sign=self.detect_sign,
+                adjacency_radius=self.adjacency_radius,
+                detect_threshold=self.detect_threshold,
+                merge_thresh=self.merge_thresh,
+                freq_min=self.freq_min,
+                freq_max=self.freq_max,
+                pc_per_chan=self.pc_per_chan
+            )
+            si.MdaSortingExtractor.writeSorting(sorting=sorting,save_path=self.firings_out)
+        except:
+            if os.path.exists(tmpdir):
+                shutil.rmtree(tmpdir)
+            raise
+        shutil.rmtree(tmpdir)
+
+#sf.sorters.ironclust(*, recording, tmpdir, detect_sign=-1, adjacency_radius=-1, detect_threshold=5, merge_thresh=0.98, freq_min=300, freq_max=6000, pc_per_chan=3, prm_template_name, ironclust_src=None)
+        
+Processors=dict(
+    MountainSort4=(MountainSort4,'sha1://a3842053423c633b62e70474be9d76068cdc1ea5/mountainsort4.simg'),
+    IronClust=(IronClust,None),
+    SpykingCircus=(SpykingCircus,'sha1://487fe664853285e65f8130a1138a4415f8acc4ca/spyking_circus.simg'),
+    KiloSort=(KiloSort,None)
+)
+        
+def sort_recording(*,sorter,recording):
+    dsdir=recording['directory']
+    sorting_params=sorter['params']
+    processor_name=sorter['processor_name']
+    if processor_name in Processors:
+        SS=Processors[processor_name][0]
+        SS_container=Processors[processor_name][1]
+    else:
+        raise Exception('No such sorter: '+processor_name)
+
+    if SS_container:
+        print('Locating container: '+SS_container)
+        if not kb.realizeFile(SS_container):
+            raise Exception('Unable to realize container: '+SS_container)
+        
+    print('Sorting recording {} using {}'.format(dsdir, processor_name))
+    X=SS.execute(
+        _container=SS_container,
+        recording_dir=dsdir,
+        channels=recording.get('channels',[]),
+        firings_out=dict(ext='.mda'),
+        **sorting_params
+    )
+    outputs=X.outputs
+    stats=X.stats
+    print('Saving firings_out...')
+    firings_out=kb.saveFile(outputs['firings_out'])
+    firings_true_path=recording['directory']+'/firings_true.mda'
+    if not kb.findFile(firings_true_path):
+        firings_true_path=None
+    print('Assembling result...')
+    result=dict(
+        recording_name=recording.get('name',None),
+        study_name=recording.get('study',None),
+        sorter_name=sorter.get('name',None),
+        recording_dir=dsdir,
+        channels=recording.get('channels',[]),
+        units_true=recording.get('units_true',[]),
+        firings_true=firings_true_path,
+        sorting_params=sorting_params,
+        sorting_processor_name=SS.NAME,
+        sorting_processor_version=SS.VERSION,
+        sorting_execution_stats=stats,
+        container=SS_container,
+        firings=firings_out
+    )
+    print('Done sorting.')
+    return result
