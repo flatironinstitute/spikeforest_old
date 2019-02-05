0fbadc2e82e40b2ca383436d1ea7671a1c564ea3
Jeremy Magland <jeremy.magland@gmail.com>
2019-02-05 19:14:30 +0000

    developing new analysis system

--- repos/vdomr/vdomr/__init__.py
+++ repos/vdomr/vdomr/__init__.py
@@ -3,6 +3,6 @@ from .component import Component
 from . import devel
 from . import components
 from .vdomr import register_callback, invoke_callback, exec_javascript, _take_javascript_to_execute, _set_server_session
-from .vdomr import config_jupyter, config_colab, config_server
+from .vdomr import config_jupyter, config_colab, config_server, mode
 
 from .vdomrserver import VDOMRServer
