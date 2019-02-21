f893a96469ae32c338865634fe9cc9943c2acaf6
Jeremy Magland <jeremy.magland@gmail.com>
2019-02-21 06:14:05 -0500

    update

--- working/compute_resources/ccmlin008-kilosort/start.sh
+++ working/compute_resources/ccmlin008-kilosort/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-set -e
+
+module load matlab
+module load cuda
 
 export NUM_WORKERS=2
 export MKL_NUM_THREADS=$NUM_WORKERS
@@ -13,4 +15,4 @@ export CAIRIO_CONFIG_PASSWORD=$SPIKEFOREST_PASSWORD
 # find containers (look in spikeforest2)
 export CAIRIO_ALTERNATE_SHARE_IDS=69432e9201d0
 
-../../../bin/batcho_listen ccmlin008-kilosort --allow_uncontainerized --parallel=1 2>&1 | tee output.txt
+../../../bin/batcho_listen ccmlin000-gpu --allow_uncontainerized  2>&1 | tee output.txt
