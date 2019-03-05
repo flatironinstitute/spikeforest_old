4943a8870bf5656bcbc1ec441f4990f3f9cded7c
Jeremy Magland <jeremy.magland@gmail.com>
2019-03-05 12:24:06 -0500

    fix issue with saving result object

--- mountaintools/mlprocessors/internal_run_batch_job.py
+++ mountaintools/mlprocessors/internal_run_batch_job.py
@@ -71,9 +71,8 @@ def run_batch_job(collection,share_id,token,upload_token,batch_id,job_index, sys
         key=dict(
             name='compute_resource_batch_job_result',
             batch_id=batch_id,
-            job_index=job_index
+            job_index=index
         )
-        print('------------------------------- save result to key:',key)
         cairio_client.saveObject(
             key=key,
             object=result
