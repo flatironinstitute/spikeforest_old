28dbc2479d48ac26a97460ba3b03b7819768142f
Jeremy Magland <jeremy.magland@gmail.com>
2019-03-05 10:01:44 -0500

    updates to processing

--- working/compute_resources/ccmlin008-80/start.sh
+++ working/compute_resources/ccmlin008-80/start.sh
@@ -11,7 +11,7 @@ export DISPLAY=""
 COLLECTION=spikeforest
 SHARE_ID=69432e9201d0
 
-../../../bin/compute-resource-start ccmlin008-default \
+../../../bin/compute-resource-start ccmlin008-80 \
 	--allow_uncontainerized  \
 	--collection $COLLECTION --share_id $SHARE_ID \
         --srun_opts "-c 2 -n 80 -p ccm"
