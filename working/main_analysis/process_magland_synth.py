f31dad5a139c83ef398cb1a2e8b516270689a547
Jeremy Magland <jeremy.magland@gmail.com>
2019-03-05 10:26:01 -0500

    fix finalize_batch bug

--- working/main_analysis/process_magland_synth.py
+++ working/main_analysis/process_magland_synth.py
@@ -20,8 +20,8 @@ def main():
 
     # Specify the compute resource (see the note above)
     # compute_resource = 'local-computer'
-    #compute_resource = dict(resource_name='ccmlin008-default',collection='spikeforest',share_id='69432e9201d0')
-    compute_resource = dict(resource_name='ccmlin008-80',collection='spikeforest',share_id='69432e9201d0')
+    compute_resource = dict(resource_name='ccmlin008-default',collection='spikeforest',share_id='69432e9201d0')
+    #compute_resource = dict(resource_name='ccmlin008-80',collection='spikeforest',share_id='69432e9201d0')
     compute_resource_ks = dict(resource_name='ccmlin008-gpu',collection='spikeforest',share_id='69432e9201d0')
 
     # Use this to control whether we force the processing to re-run (by default it uses cached results)
