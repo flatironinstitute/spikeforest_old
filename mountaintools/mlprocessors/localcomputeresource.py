98d7842970939a797c7c908bbed6cce0b8979b7f
jamesjun <jamesjun@gmail.com>
2019-03-28 09:30:32 -0400

    local compute resource can use srun options (srun_opts)

--- mountaintools/mlprocessors/localcomputeresource.py
+++ mountaintools/mlprocessors/localcomputeresource.py
@@ -4,13 +4,15 @@ import mlprocessors as mlpr
 import multiprocessing
 import random
 class LocalComputeResource():
-    def __init__(self,num_parallel):
+    def __init__(self,num_parallel,srun_opts=None):
         self._num_parallel=num_parallel
         self._exit_event = multiprocessing.Event()
+        self._srun_opts = srun_opts
+
     def __enter__(self):
         from mountaintools import client as mt
         resource_name='local_resource_'+_random_string(6)
-        self._process=multiprocessing.Process(target=_run_local_compute_resource, args=(resource_name,self._num_parallel, self._exit_event))
+        self._process=multiprocessing.Process(target=_run_local_compute_resource, args=(resource_name,self._num_parallel, self._exit_event, self._srun_opts))
         self._process.start()
         
         return dict(resource_name=resource_name,collection=None,share_id=None)
@@ -25,7 +27,7 @@ class LocalComputeResource():
 def _random_string(num):
     return ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', k=num))
 
-def _run_local_compute_resource(resource_name, num_parallel, exit_event):
+def _run_local_compute_resource(resource_name, num_parallel, exit_event, srun_opts=None):
     import mlprocessors as mlpr
     from mountaintools import client as mt
     server=mlpr.ComputeResourceServer(
@@ -34,6 +36,8 @@ def _run_local_compute_resource(resource_name, num_parallel, exit_event):
         share_id=None
     )
     server.setNumParallel(num_parallel)
+    if srun_opts is not None:
+        server.setSrunOptsString(srun_opts)
     server.start(exit_event=exit_event)
 
 # with LocalComputeResource(num_parallel=4) as compute_resource:
