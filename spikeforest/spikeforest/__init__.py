fd122c429f21b9c8a6cf493ad54a79c460274eae
Jeremy Magland <jeremy.magland@gmail.com>
2019-04-18 06:20:27 -0400

    add back in neuroscope sorting extractor

--- spikeforest/spikeforest/__init__.py
+++ spikeforest/spikeforest/__init__.py
@@ -1,4 +1,5 @@
 from .extractors import SFMdaRecordingExtractor, SFMdaSortingExtractor, mdaio
 from .extractors import EfficientAccessRecordingExtractor
 from .extractors import loadProbeFile, saveProbeFile, writeBinaryDatFormat, getSubExtractorsByProperty
+from .extractors import NeuroscopeSortingExtractor
 from . import example_datasets
