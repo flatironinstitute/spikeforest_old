f6842389d0817bd9a215cd8eaf8c5f41fb99b26d
Jeremy Magland <jeremy.magland@gmail.com>
2019-04-23 08:39:55 -0400

    update working analysis

--- working/prepare_recordings/prepare_visapy_synth_recordings.py
+++ working/prepare_recordings/prepare_visapy_synth_recordings.py
@@ -1,30 +1,30 @@
-import sfdata as sf
+#!/usr/bin/env python
+
 from mountaintools import client as mt
 
 mt.login()
-upload_to = 'spikeforest.spikeforest2'
+upload_to = 'kbucket'
+
 
 # The base directory used below
 basedir = 'kbucket://15734439d8cf/groundtruth'
 
-group_name = 'visapy_mea'
+group_name = 'visapy_synth'
 
-def prepare_visapy_mea_studies(*, basedir):
-    study_set_name = 'visapy_mea'
-    study_dir0 = basedir+'/visapy_mea'
-    study_dir = mt.createSnapshot(study_dir0, upload_to='spikeforest.spikeforest2', upload_recursive=False, download_recursive=False)
-    if not study_dir:
-        raise Exception('Failed to create snapshot of directory: '+study_dir0)
-    study_dir=study_dir+'.visapy_mea'
-    print('Using study dir: '+study_dir)
+def prepare_visapy_synth_studies(*, basedir):
+    study_set_name = 'visapy_synth'
+    study_set_dir0 = basedir+'/visapy_synth'
+    study_set_dir = mt.createSnapshot(study_set_dir0, upload_to=upload_to, upload_recursive=False, download_recursive=False)
+    if not study_set_dir:
+        raise Exception('Failed to create snapshot of study set directory: '+study_set_dir0)
+    study_set_dir=study_set_dir+'.visapy_synth'
+    print('Using study set dir: '+study_set_dir)
     studies = []
     recordings = []
-    names = []
-    names = names+['visapy_mea']
-    for name in names:
-        print('PREPARING: '+name)
-        study_name = 'visapy_mea'
-        study_dir = study_dir
+    study_names = ['mea_c30']
+    for study_name in study_names:
+        print('PREPARING: '+study_name)
+        study_dir = study_set_dir+'/'+study_name
         study0 = dict(
             name=study_name,
             study_set=study_set_name,
@@ -47,7 +47,7 @@ def prepare_visapy_mea_studies(*, basedir):
 
 
 # Prepare the studies
-studies, recordings = prepare_visapy_mea_studies(basedir=basedir)
+studies, recordings = prepare_visapy_synth_studies(basedir=basedir)
 print('Saving object...')
 address = mt.saveObject(
     object=dict(
