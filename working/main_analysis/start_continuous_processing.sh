e2f2bd71f14c7d96e6f7807362e8e84875781675
Jeremy Magland <jeremy.magland@gmail.com>
2019-04-25 06:22:07 -0400

    update continuous processing script

--- working/main_analysis/start_continuous_processing.sh
+++ working/main_analysis/start_continuous_processing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 declare -a list=("visapy_synth" "magland_synth" "mearec_tetrode", "manual_tetrode")
 
@@ -6,13 +7,12 @@ while true
 do
     for item in "${list[@]}"
     do 
-        echo "============================================================================="
-        echo "== ${item}"
-        echo "============================================================================="
-        ./spikeforest_analysis analysis.${item}.json > ${item}.0.log
+        echo "=============================================================================" | tee ${item}.0.log
+        echo "$(date): Processing ${item} -- see ${item}.0.log" | tee -a ${item}.0.log
+        echo "=============================================================================" | tee -a ${item}.0.log
+        ./spikeforest_analysis analysis.${item}.json >> ${item}.0.log
         cat ${item}.0.log >> ${item}.log
-        echo ""
-        echo ""
+        cat ${item}.0.log >> continuous.processing.log
         echo ""
 
     done
