147bdcd1df6634fe932295c8af851c8d1c32ac64
Jeremy Magland <jeremy.magland@gmail.com>
2019-04-29 08:07:10 -0400

    rename analysis file

--- working/prepare_recordings/prepare_all.sh
+++ working/prepare_recordings/prepare_all.sh
@@ -6,7 +6,6 @@ set -e
 ./prepare_synth_visapy_recordings.py
 ./prepare_paired_recordings.py
 ./prepare_synth_mearec_neuronexus_recordings.py
-./prepare_toy_recordings_local.py
 # ./prepare_hybrid_janelia_recordings.py
 ./prepare_synth_bionet_recordings.py
 ./prepare_synth_mearec_tetrode_recordings.py
