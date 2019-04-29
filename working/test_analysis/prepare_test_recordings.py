13d43e095b9aa6d953630232e35a1047ee994472
Jeremy Magland <jeremy.magland@gmail.com>
2019-04-29 09:05:39 -0400

    preparing test analyses

--- working/test_analysis/prepare_test_recordings.py
+++ working/test_analysis/prepare_test_recordings.py
@@ -4,19 +4,22 @@ from mountaintools import client as mt
 
 mt.configDownloadFrom('spikeforest.kbucket')
 
-X = mt.loadObject(path="key://pairio/spikeforest/spikeforest_recording_group.synth_magland.json")
-study_name = "synth_magland_noise10_K10_C4"
-studies = [y for y in X['studies'] if (y['name']==study_name)]
-recordings = [y for y in X['recordings'] if y['study']==study_name]
-recordings = recordings[0:1]
-study_sets = X['study_sets']
+def do_prepare(recording_group, study_name):
+    print(recording_group, study_name)
+    X = mt.loadObject(path="key://pairio/spikeforest/spikeforest_recording_group.{}.json".format(recording_group))
+    studies = [y for y in X['studies'] if (y['name']==study_name)]
+    recordings = [y for y in X['recordings'] if y['study']==study_name]
+    recordings = recordings[0:1]
+    study_sets = X['study_sets']
 
-Y = dict(
-    studies=studies,
-    recordings=recordings,
-    study_sets=study_sets
-)
-address = mt.saveObject(object=Y)
-assert address is not None
-print(address)
-mt.createSnapshot(path=address, upload_to='spikeforest.kbucket', dest_path='key://pairio/spikeforest/spikeforest_recording_group.test_synth_magland.json')
+    Y = dict(
+        studies=studies,
+        recordings=recordings,
+        study_sets=study_sets
+    )
+    address = mt.saveObject(object=Y)
+    assert address is not None
+    mt.createSnapshot(path=address, upload_to='spikeforest.kbucket', dest_path='key://pairio/spikeforest/spikeforest_recording_group.test_{}.json'.format(recording_group))
+
+do_prepare(recording_group='synth_magland', study_name='synth_magland_noise10_K10_C4')
+do_prepare(recording_group='paired_mea64c', study_name='paired_mea64c')
