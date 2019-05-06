e9c919760c6d44b10e01295305a60570517d42a5
Jeremy Magland <jeremy.magland@gmail.com>
2019-05-06 15:21:45 -0400

    update containers!

--- spikeforest/spikesorters/mountainsort4/container/upload_singularity_container.py
+++ spikeforest/spikesorters/mountainsort4/container/upload_singularity_container.py
@@ -3,5 +3,5 @@
 from mountaintools import client as mt
 
 mt.login()
-sha1_path = mt.saveFile('mountainsort4.simg', upload_to='spikeforest.spikeforest2')
+sha1_path = mt.saveFile('mountainsort4.simg', upload_to='spikeforest.kbucket')
 print(sha1_path)
