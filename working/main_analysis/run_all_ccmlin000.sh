dcc6cc253a5057f6c2e157ad6cd605781919f27a
James Jun <jamesjun@gmail.com>
2019-05-16 18:29:49 -0400

    klusta added for all studyset run_all_ccmlin000.sh and run_all_ccimlin008.sh added

---
+++ working/main_analysis/run_all_ccmlin000.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+declare -a list=("paired_boyden32c" "paired_crcns" "paired_kampff" "paired_mea64c" "synth_visapy" "synth_magland" "synth_mearec_tetrode" "manual_franklab" "synth_bionet" "synth_mearec_neuronexus")
+
+OPTS="--skip_failing --compute_resources compute_resources_ccmlin000.json"
+
+for item in "${list[@]}"
+do 
+    echo "=============================================================================" | tee all.${item}.0.log
+    echo "$(date): Processing ${item} -- see all.${item}.0.log" | tee -a all.${item}.0.log
+    echo "=============================================================================" | tee -a all.${item}.0.log
+    ./spikeforest_analysis analysis.${item}.json $OPTS >> all.${item}.0.log
+    cat all.${item}.0.log >> all.${item}.log
+    cat all.${item}.0.log >> all.processing.log
+    echo ""
+
+done
+
