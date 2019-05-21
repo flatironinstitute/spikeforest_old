ab36e5baf55fee0a0f2b1a83416641c203560c1f
Jeremy Magland <jeremy.magland@gmail.com>
2019-05-21 04:20:27 -0400

    update prepare all

--- working/prepare_recordings/prepare_all.sh
+++ working/prepare_recordings/prepare_all.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-./prepare_manual_franklab_recordings.py
-./prepare_synth_magland_recordings.py
-./prepare_synth_visapy_recordings.py
-./prepare_paired_recordings.py
-./prepare_synth_mearec_neuronexus_recordings.py
-# ./prepare_hybrid_janelia_recordings.py
-./prepare_synth_bionet_recordings.py
-./prepare_synth_mearec_tetrode_recordings.py
+prepare_hybrid_janelia_recordings.py
+prepare_manual_franklab_recordings.py
+prepare_neurocube_single_recordings.py
+prepare_paired_recordings.py
+prepare_synth_bionet_recordings.py
+prepare_synth_magland_recordings.py
+prepare_synth_mearec_neuronexus_recordings.py
+# prepare_synth_mearec_sqmea64c_recordings.py
+prepare_synth_mearec_tetrode_recordings.py
+prepare_synth_visapy_recordings.py
