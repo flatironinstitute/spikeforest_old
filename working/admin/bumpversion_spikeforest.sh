d0f09d56cd895aa7e17b001087a49022ddec72a8
Jeremy Magland <jeremy.magland@gmail.com>
2019-06-08 05:15:29 -0400

    add admin tools

---
+++ working/admin/bumpversion_spikeforest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd `dirname "$0"`/../../spikeforest
+
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  if [ -z "$1" ]; then
+    echo "You must supply an option, e.g., patch, minor, major"
+    exit 0
+  fi
+  if [ "$2" == "go" ]; then
+    bumpversion $1 --verbose
+    echo "Now you should push via 'git push && push --tags' and replace the explicit version in all the docs."
+  else
+    bumpversion $1 --dry-run --verbose
+    echo "That was a dry run. If it looks okay, then add the 'go' argument"
+  fi
+else 
+  echo "Working directory is not clean:"
+  echo "$output"
+fi
