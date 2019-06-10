887f637a7a3cb0cba1452868f54caa54407573fb
James Jun <jamesjun@gmail.com>
2019-06-10 14:51:50 -0400

    [4.6.7] - 2019-6-10 Changed - `fft_thresh=6` (previously 10).   - Detrending of the power is done using sliding median filter (medfilt1.m)

--- spikeforest/spikesorters/ironclust/ironclust.py
+++ spikeforest/spikesorters/ironclust/ironclust.py
@@ -19,7 +19,7 @@ from .install_ironclust import install_ironclust
 
 class IronClust(mlpr.Processor):
     NAME = 'IronClust'
-    VERSION = '0.4.0'
+    VERSION = '0.4.1'
     ENVIRONMENT_VARIABLES = [
         'NUM_WORKERS', 'MKL_NUM_THREADS', 'NUMEXPR_NUM_THREADS', 'OMP_NUM_THREADS', 'TEMPDIR']
     ADDITIONAL_FILES = ['*.m']
@@ -88,7 +88,7 @@ class IronClust(mlpr.Processor):
     fGpu = mlpr.BoolParameter(
         optional=True, default=True, description='Use GPU if available')
     fft_thresh = mlpr.FloatParameter(
-        optional=True, default=10, description='FFT-based noise peak threshold')
+        optional=True, default=6, description='FFT-based noise peak threshold')
     nSites_whiten = mlpr.IntegerParameter(
         optional=True, default=32, description='Number of adjacent channels to whiten')
     feature_type = mlpr.StringParameter(
@@ -97,7 +97,7 @@ class IronClust(mlpr.Processor):
     @staticmethod
     def install():
         print('Auto-installing ironclust.')
-        return install_ironclust(commit='3a6878e5a0f0fad634430e6ddd1c8e711b29158a')
+        return install_ironclust(commit='2da8d885dc4101a596ef13f7b61ef7dfa197803b')
 
     def run(self):
         ironclust_path = os.environ.get('IRONCLUST_PATH_DEV', None)
