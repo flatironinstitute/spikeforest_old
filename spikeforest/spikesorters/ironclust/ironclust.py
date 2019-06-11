07a540dac32a6d2af73dcb96111703cbc6b4aeb3
James Jun <jamesjun@gmail.com>
2019-06-11 12:29:55 -0400

    IronClust updated

--- spikeforest/spikesorters/ironclust/ironclust.py
+++ spikeforest/spikesorters/ironclust/ironclust.py
@@ -19,7 +19,7 @@ from .install_ironclust import install_ironclust
 
 class IronClust(mlpr.Processor):
     NAME = 'IronClust'
-    VERSION = '0.4.2b'
+    VERSION = '0.4.3a'
     ENVIRONMENT_VARIABLES = [
         'NUM_WORKERS', 'MKL_NUM_THREADS', 'NUMEXPR_NUM_THREADS', 'OMP_NUM_THREADS', 'TEMPDIR']
     ADDITIONAL_FILES = ['*.m']
@@ -88,7 +88,7 @@ class IronClust(mlpr.Processor):
     fGpu = mlpr.BoolParameter(
         optional=True, default=True, description='Use GPU if available')
     fft_thresh = mlpr.FloatParameter(
-        optional=True, default=6, description='FFT-based noise peak threshold')
+        optional=True, default=10, description='FFT-based noise peak threshold')
     nSites_whiten = mlpr.IntegerParameter(
         optional=True, default=32, description='Number of adjacent channels to whiten')
     feature_type = mlpr.StringParameter(
@@ -97,7 +97,7 @@ class IronClust(mlpr.Processor):
     @staticmethod
     def install():
         print('Auto-installing ironclust.')
-        return install_ironclust(commit='e1bde30d1fc5948fc7802bcca363c46117fd7272')
+        return install_ironclust(commit='aa331249537e0e19f3f21fec45c4ae864bff24ff')
 
     def run(self):
         ironclust_path = os.environ.get('IRONCLUST_PATH_DEV', None)
---
+++ working/main_analysis/run_all_irc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+ARGS="main_analysis_irc.json --use_slurm" # --skip_failing"
+
+./main_analysis $ARGS --analyses hybrid_janelia
+./main_analysis $ARGS --analyses manual_franklab
+./main_analysis $ARGS --analyses paired_boyden32c
+./main_analysis $ARGS --analyses paired_crcns
+./main_analysis $ARGS --analyses paired_kampff
+./main_analysis $ARGS --analyses paired_mea64c
+./main_analysis $ARGS --analyses paired_monotrode
+./main_analysis $ARGS --analyses synth_bionet
+./main_analysis $ARGS --analyses synth_magland
+./main_analysis $ARGS --analyses synth_mearec_neuronexus
+./main_analysis $ARGS --analyses synth_mearec_tetrode
+./main_analysis $ARGS --analyses synth_monotrode
+./main_analysis $ARGS --analyses synth_visapy
