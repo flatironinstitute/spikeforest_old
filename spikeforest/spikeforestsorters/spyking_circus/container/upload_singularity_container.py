694641df531e0f65a4948edbbb8abdc272d9d184
Jeremy Magland <jeremy.magland@gmail.com>
2019-07-22 06:45:30 -0400

    update spyking circus wrapper to 0.8.3

--- spikeforest/spikeforestsorters/spyking_circus/container/upload_singularity_container.py
+++ spikeforest/spikeforestsorters/spyking_circus/container/upload_singularity_container.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
 from mountaintools import client as mt
+
+sha1_path = mt.saveFile('spyking_circus.simg')
+print(sha1_path)
 sha1_path = mt.saveFile('spyking_circus.simg', upload_to='spikeforest.public')
 print(sha1_path)
