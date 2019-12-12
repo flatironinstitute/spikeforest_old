cc35285284780326ce8090337ed8b9891d31ccad
James Jun <jamesjun@gmail.com>
2019-12-12 14:26:54 -0500

    ironclust updated

--- spikeforest/spikeforestsorters/ironclust/ironclust.py
+++ spikeforest/spikeforestsorters/ironclust/ironclust.py
@@ -16,7 +16,7 @@ from .install_ironclust import install_ironclust
 
 class IronClust(mlpr.Processor):
     NAME = 'IronClust'
-    VERSION = '0.8.10'
+    VERSION = '0.8.11'
     ENVIRONMENT_VARIABLES = [
         'NUM_WORKERS', 'MKL_NUM_THREADS', 'NUMEXPR_NUM_THREADS', 'OMP_NUM_THREADS', 'TEMPDIR']
     CONTAINER: Union[str, None] = None
@@ -101,7 +101,7 @@ class IronClust(mlpr.Processor):
     @staticmethod
     def install():
         print('Auto-installing ironclust.')
-        return install_ironclust(commit='9a7f5e37e1ae99dec66256cb8a276aa5c61ce721')
+        return install_ironclust(commit='38e876ffaa4d494ad7a8c3d52a231d947d270ff7')
 
     def run(self):
         timer = time.time()
